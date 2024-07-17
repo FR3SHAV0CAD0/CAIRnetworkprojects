1ca0088417fe276c58d50564e1be67d24a6ebc8f
FR3SHAV0CAD0 <cmneilso@ualberta.ca>
2024-07-17 03:33:36 -0600

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const map = new maplibregl.Map({
-        container: 'map',
-        style: 'https://demotiles.maplibre.org/style.json', // Using MapLibre's demo style
-        center: [-106.3468, 56.1304],
-        zoom: 3
+const map = new maplibregl.Map({
+    container: 'map',
+    style: 'https://api.maptiler.com/maps/streets/style.json?key=get_your_own_OpIi9ZULNHzrESv6T2vL',
+    center: [-104.136610, 49.013827],
+    zoom: 3.5
     });
 
 document.getElementById('zoom-in').addEventListener('click', () => {
